53c99f74972132168d20c12aa5625af1ba7eddb3
tGUhh <tiagocostasantos0013@gmail.com>
2023-09-01 10:43:35 -0300

    readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Variaveis
-const apiKey = process.env.local.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
 // Seleção de Elementos
@@ -26,45 +26,39 @@ const loader = document.querySelector("#loader");
 // Funções
 // acessa os dados da api
 const getWeatherData = async (city) => {
-    // exibe o loader
+   
     toggleLoader();
-    // 1 - constroi o link para consultar na api  
+ 
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-    // 2 - consulta o link da api utilizando fetch passado a variavel apiWeatherURL como argumento
     const res = await fetch(apiWeatherURL);
-    // 4 - converte os dados vindo de res em json
     const data = await res.json();
-    // oculta o loader
+
     toggleLoader();
 
     return data;
 };
 
-// exibe a mensagem de erro
 function showErrorMessage() {
     errorMessageContainer.classList.remove("hide");
 };
 
-// esconde o conteudo e mensage de erro
 function hideContent() {
     errorMessageContainer.classList.add("hide");
     weatherDataContainer.classList.add("hide");
 };
 
-// exibe o conteudo e mensage de erro
 function showContent() {
     weatherDataContainer.classList.remove("hide");
 };
-// oculta o loader
+
 function toggleLoader() {
     loader.classList.toggle("hide");
 };
-// Exibe os dados na tela
 const showweatherData = async (city) => {
     hideContent();
 
     const data = await getWeatherData(city);
-    // trantado erro cidade não encontrada
+
     if (data.cod === "404" || data.cod === "400") {
         showErrorMessage();
         return
